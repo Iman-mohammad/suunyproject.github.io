dc33ef977f4fa672dd8a1e986ae58793f0e1d4fb
Iman-mohammad <91008629+Iman-mohammad@users.noreply.github.com>
2022-02-13 11:17:06 +0200

    craete resourses file with all important files

---
+++ resourses/script/script-name.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("test").style.display = "block";
+  }
+
+  function myFunction2() {
+    document.getElementById("test").style.display = "none";
+  }
