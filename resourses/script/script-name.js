fa21794d38eec72ba19604641a34ed0b9b77d6fe
Iman-mohammad <91008629+Iman-mohammad@users.noreply.github.com>
2022-02-15 13:28:14 +0200

    update onclick attribute

--- resourses/script/script-name.js
+++ resourses/script/script-name.js
@@ -1,7 +1,17 @@
 function myFunction() {
+    document.getElementById("hamburger-img").style.display = "none";
+    document.getElementById("cancel-img").style.display = "block";
+    
     document.getElementById("phone-list").style.display = "block";
+    document.getElementById("triangle-id").style.display = "block";
+    
+
   }
 
   function myFunction2() {
     document.getElementById("phone-list").style.display = "none";
+    document.getElementById("triangle-id").style.display = "none";
+    document.getElementById("hamburger-img").style.display = "block";
+    document.getElementById("cancel-img").style.display = "none";
+
   }
