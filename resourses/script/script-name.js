2ef82c4339a5d28e879a94b72786041bfe149771
Iman-mohammad <91008629+Iman-mohammad@users.noreply.github.com>
2022-02-15 13:47:25 +0200

    update function names , completed

--- resourses/script/script-name.js
+++ resourses/script/script-name.js
@@ -1,14 +1,11 @@
-function myFunction() {
+function phoneheader() {
     document.getElementById("hamburger-img").style.display = "none";
     document.getElementById("cancel-img").style.display = "block";
-    
     document.getElementById("phone-list").style.display = "block";
     document.getElementById("triangle-id").style.display = "block";
-    
-
   }
 
-  function myFunction2() {
+  function desktopheader() {
     document.getElementById("phone-list").style.display = "none";
     document.getElementById("triangle-id").style.display = "none";
     document.getElementById("hamburger-img").style.display = "block";
